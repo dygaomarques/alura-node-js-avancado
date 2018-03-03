52a36d4814d28719dcb3b40248a00f847ebb2a1a
Dygão <dygaomail@gmail.com>
2018-03-02 21:55:21 -0300

    Implementing HATEOAS

--- controllers/payaments.js
+++ controllers/payaments.js
@@ -54,11 +54,31 @@ module.exports = ( app ) => {
 
           console.log( result );
 
+          /* Defining the payament id */
+          payament.id = result.insertId;
+
           /* Return the location of new resource */
-          res.location( `payaments/payament/${result.insertId}` );
+          res.location( `payaments/payament/${payament.id}` );
+
+          /* Response + HATEOAS */
+          let response = {
+            payament: payament,
+            links: [
+              {
+                href:   "http://localhost:3000/payaments/payament/" + payament.id,
+                rel:    "Confirm payament",
+                method: "PUT"
+              },
+              {
+                href:   "http://localhost:3000/payaments/payament/" + payament.id,
+                rel:    "Cancel payament",
+                method: "DELETE"
+              }
+            ]
+          }
           
           /* Return the payament status */
-          res.status( 201 ).send( { message: "Payament created!", payament } );
+          res.status( 201 ).send( response );
 
         }).catch( ( err ) => {
 
@@ -89,7 +109,40 @@ module.exports = ( app ) => {
           message:  message,
           result:   data
         });
+
         res.status( 200 ).send({ message: message });
+
+      }).catch(( error ) => {
+
+        res.status( 500 ).send({ error });
+
+      });
+
+  });
+
+  /**
+   * Route to delete a payament
+   */
+  app.delete( '/payaments/payament/:id', ( req, res ) => {
+
+    let payament = {};
+
+    payament.status = 'CANCELLED';
+    payament.id = req.params.id;
+
+    /* Create new connection */
+    let connection = app.services.ConnectionFactory.connection();
+
+    new app.dao.PayamentsDao( connection ).update( payament )
+      .then(( data ) => {
+        let message = `Payament ${ payament.id } cancelled!`;
+
+        console.log({
+          message:  message,
+          result:   data
+        });
+        
+        res.status( 204 ).send({ message: message });
 
       }).catch(( error ) => {
 
